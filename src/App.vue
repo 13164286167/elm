c2334b8589c2f28f2eb471becc32d04dea4a0ec7
tangboo <13164286167@163.com>
2018-11-09 19:09:13 +0800

    elm

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div class="app_header">
+     <Header></Header>
+    </div>
+    <div class="nav">
+      <div class="nav-item">
+        <a link="{path:'/goods'}">商品</a>
+        
+      </div>
+      <div class="nav-item">
+        <a link="{path:'/ratings'}">评论</a>
+        
+      </div>
+      <div class="nav-item">
+        <a v-link="{path:'/seller'}">商家</a>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Header from './components/header/header.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    Header
+  }
 }
 </script>
 
-<style>
+<style rel="./common/style/reset.css">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,4 +39,15 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+#app .nav{
+  display: flex;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+}
+#app .nav .nav-item{
+  flex: 1;
+  text-align: center;
+}
+
 </style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="head">
+        我是header
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data(){
+        return {
+
+        }
+    },
+}
+</script>
+
+<style>
+    
+</style>
+
+
